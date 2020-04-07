457c40bfe5863a5d83becd40cf8dd579f7d5007c
Mirko Oleszuk <mirko@oleszuk.de>
2020-04-07 10:53:35 +0200

    + testing

---
+++ tests/test.php
@@ -0,0 +1,13 @@
+<?php
+
+// ha ha, don't expect real tests here
+
+require_once __DIR__ . "/../vendor/autoload.php";
+require_once __DIR__ . "/../public/restapi.php";
+require_once __DIR__ . "/../public/log.php";
+require_once __DIR__ . "/../public/helper.php";
+require_once __DIR__ . "/../public/stats.php";
+
+#echo get_world_status();
+
+#echo get_country_status('DE');
