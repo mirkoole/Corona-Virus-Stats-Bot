9bf6fedb23439335b6a9bf8747acb7521cf1e0ac
Mirko Oleszuk <mirko@oleszuk.de>
2020-07-14 08:43:37 +0200

    * improved chart readability

--- public/image_active.php
+++ public/image_active.php
@@ -31,16 +31,18 @@ foreach ($result as $e) {
     $key = $date;
     $value = (int)$e['Active'];
     $value_d = (int)$e['Deaths'];
-    $value_r = (int)$e['Recovered'];
+    //$value_r = (int)$e['Recovered'];
 
     // improves chart readability
-    if (($value + $value_d + $value_r) > 100) {
+    //if (($value + $value_d + $value_r) > 100) {
+    if (($value + $value_d) > 100) {
         $data[$key]['Active'] = $value;
         $data[$key]['Deaths'] = $value_d;
-        $data[$key]['Recovered'] = $value_r;
+        //$data[$key]['Recovered'] = $value_r;
 
         // calc max val
-        $max = max($max, $value, $value_d, $value_r);
+        //$max = max($max, $value, $value_d, $value_r);
+        $max = max($max, $value, $value_d);
 
     }
 
@@ -51,8 +53,8 @@ foreach ($result as $e) {
  */
 
 // Image dimensions
-$imageWidth = 800;
-$imageHeight = 600;
+$imageWidth = 1280;
+$imageHeight = 720;
 
 // Grid dimensions and placement within image
 $gridTop = 50;
@@ -72,7 +74,7 @@ $font = __DIR__ . '/fonts/Verdana.ttf';
 $fontSize = 10;
 
 // Margin between label and axis
-$labelMargin = 8;
+$labelMargin = 12;
 
 // Distance between grid lines on y-axis
 $yLabelSpan = 5000;
@@ -84,10 +86,14 @@ $yMaxValue = $max + $yLabelSpan;
 // Max value on y-axis
 if ($yMaxValue > 10000) {
     $yMaxValue = round($max * 1.1, -4);
-}
 
-// Distance between grid lines on y-axis
-$yLabelSpan = round($max * 0.1, -3);
+    // Distance between grid lines on y-axis
+    $yLabelSpan = round($max * 0.1, -4);
+} else {
+    // Distance between grid lines on y-axis
+    $yLabelSpan = round($max * 0.1, -3);
+
+}
 
 
 // Init image
@@ -100,7 +106,7 @@ $labelColor = $axisColor;
 $gridColor = imagecolorallocate($chart, 212, 212, 212);
 $barColor_red = imagecolorallocate($chart, 217, 47, 47); // active
 $barColor_black = imagecolorallocate($chart, 47, 47, 47); // death
-$barColor_blue = imagecolorallocate($chart, 47, 133, 217); // recovered
+//$barColor_blue = imagecolorallocate($chart, 47, 133, 217); // recovered
 
 imagefill($chart, 0, 0, $backgroundColor);
 
@@ -149,11 +155,12 @@ foreach ($data as $key => $value) {
     $x1 = $itemX - $barWidth / 2;
     $y1 = $gridBottom - $value['Active'] / $yMaxValue * $gridHeight;
     $y1_death = $gridBottom - $value['Deaths'] / $yMaxValue * $gridHeight;
-    $y1_recovered = $gridBottom - $value['Recovered'] / $yMaxValue * $gridHeight;
+    //$y1_recovered = $gridBottom - $value['Recovered'] / $yMaxValue * $gridHeight;
     $x2 = $itemX + $barWidth / 2;
     $y2 = $gridBottom - 1;
 
     // "sort" bars
+    /*
     if ($y1 < $y1_recovered) {
         imagefilledrectangle($chart, $x1, $y1, $x2, $y2, $barColor_red);
         imagefilledrectangle($chart, $x1, $y1_recovered, $x2, $y2, $barColor_blue);
@@ -161,8 +168,10 @@ foreach ($data as $key => $value) {
         imagefilledrectangle($chart, $x1, $y1_recovered, $x2, $y2, $barColor_blue);
         imagefilledrectangle($chart, $x1, $y1, $x2, $y2, $barColor_red);
     }
+    */
 
-    if($y1 < $y1_death) {
+    if ($y1 < $y1_death) {
+        imagefilledrectangle($chart, $x1, $y1, $x2, $y2, $barColor_red);
         imagefilledrectangle($chart, $x1, $y1_death, $x2, $y2, $barColor_black);
     } else {
         imagefilledrectangle($chart, $x1, $y1_death, $x2, $y2, $barColor_black);
@@ -178,7 +187,7 @@ foreach ($data as $key => $value) {
     $labelY = $gridBottom + $labelMargin + $fontSize;
 
     if ($i % $m == 0) {
-        if ($l % 2 != 0) $labelY += 15;
+        #if ($l % 2 != 0) $labelY += 15;
         $l++;
         imagettftext($chart, $fontSize, 0, $labelX, $labelY, $labelColor, $font, $key);
     }
@@ -188,11 +197,11 @@ foreach ($data as $key => $value) {
     $i++;
 }
 
-imagettftext($chart, 14, 0, 250, 30, $labelColor, $font, "COVID-19 Cases " . ucwords($country, "-"));
-imagettftext($chart, 7, 0, 250, 45, $labelColor, $font, "via Telegram Bot @CoronananaVirusBot powered by covid19api.com");
-imagettftext($chart, 11, 0, 200, 70, $barColor_red, $font, "Active = RED");
-imagettftext($chart, 11, 0, 350, 70, $barColor_black, $font, "Death = BLACK");
-imagettftext($chart, 11, 0, 500, 70, $barColor_blue, $font, "Recovered = BLUE");
+imagettftext($chart, 14, 0, 450, 30, $labelColor, $font, "COVID-19 Cases " . ucwords($country, "-"));
+imagettftext($chart, 7, 0, 450, 45, $labelColor, $font, "via Telegram Bot @CoronananaVirusBot powered by covid19api.com");
+imagettftext($chart, 11, 0, 450, 70, $barColor_red, $font, "Active = RED");
+imagettftext($chart, 11, 0, 600, 70, $barColor_black, $font, "Death = BLACK");
+//imagettftext($chart, 11, 0, 500, 70, $barColor_blue, $font, "Recovered = BLUE");
 
 /*
  * Output image to browser
