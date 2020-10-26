fe3383a561a2f9e5a6b20b330725be5ae8600301
Mirko Oleszuk <mirko@oleszuk.de>
2020-10-26 01:53:20 +0100

    *

--- tests/test.php
+++ tests/test.php
@@ -4,12 +4,12 @@
 
 require_once __DIR__ . "/../vendor/autoload.php";
 require_once __DIR__ . "/../public/restapi.php";
-#require_once __DIR__ . "/../public/settings.php";
+require_once __DIR__ . "/../public/settings.php";
 require_once __DIR__ . "/../public/log.php";
 require_once __DIR__ . "/../public/helper.php";
 require_once __DIR__ . "/../public/stats.php";
 
 #echo get_country_history($country = 'brazil', 'BR', $history = 30);
 #echo get_country_history_table($country = 'germany', $history = 30);
-#echo get_country_status($countryCode = 'DE');
-#echo get_world_status(get_world_status_data());
+echo get_country_status($countryCode = 'DE');
+#echo get_world_status();
